229a10e9b48ddbdb722abb355d61100c4bd1068f
TUNERXD <127730775+TUNERXD@users.noreply.github.com>
2025-09-29 23:14:06 +0700

    Make basic GUI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,93 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{Element, Theme};
+use iced::widget::{column, row, container, button, text, horizontal_space, text_input, combo_box};
+
+
+fn main() -> iced::Result {
+    iced::run("Tarner Monitor - Process Monitor & Manager", TarnerMonitor::update, TarnerMonitor::view)
 }
+
+struct TarnerMonitor;
+
+impl Default for TarnerMonitor {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Test,
+    TestInput(String),
+}
+
+struct Search {
+    placeholder: String,
+}
+
+#[derive(Debug, Clone)]
+struct Filter {
+    option: combo_box::State<FilterOptions>,
+}
+
+#[derive(Debug, Clone)]
+enum FilterOptions {
+    One,
+}
+
+struct Refresh;
+
+impl TarnerMonitor {
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Test => println!("Test"),
+            _ => println!("Test Input"),
+        }
+    }
+
+    fn view(&self) -> Element<'_, Message, Theme> {
+
+        let search = Search {
+            placeholder: String::from("Type to search..."),
+        };
+        
+        let header = container(
+            row![
+                text("TarnerMonitor"),
+                horizontal_space(),
+                button("Settings").on_press(Message::Test),
+                button("Export").on_press(Message::Test),
+            ]
+            .spacing(10)
+        )
+        .padding(10);
+
+        let sfr = container(
+            row![
+                horizontal_space(),
+                text("Search: "),
+                text_input("Search", &search.placeholder).on_input(move |input: String| Message::TestInput(input)),
+                horizontal_space(),
+                button("End Task").on_press(Message::Test),
+                horizontal_space(),
+                text("Filter: "),
+                horizontal_space(),
+                text("Refresh: "),
+            ]
+        )
+        .padding(10);
+
+        column![
+            header,
+            sfr
+        ]
+        .into()
+    }
+}
+
+
+
