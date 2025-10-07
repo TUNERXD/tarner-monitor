c1696e64896da5e6820574694f37fa007d101a8c
TUNERXD <127730775+TUNERXD@users.noreply.github.com>
2025-10-07 20:19:55 +0700

    Revamped main.rs for Backend: Basic backend implementation

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,112 @@
-use iced::{Element, Theme};
-use iced::widget::{column, row, container, button, text, horizontal_space, text_input, combo_box};
+use std::ffi::OsString;
+use sysinfo::{ Pid, System};
+use std::thread;
 
-
-fn main() -> iced::Result {
-    iced::run("Tarner Monitor - Process Monitor & Manager", TarnerMonitor::update, TarnerMonitor::view)
+//TODO: Kill selected process + confirm
+ 
+struct TarnerMonitor {
+    processes: Vec<ProcessInfo>,
+    selected_process: Option<Pid>,
+    seach_str: String,
+    total_memory: u64,
+    cpu_len: usize,
 }
-
-struct TarnerMonitor;
-
-impl Default for TarnerMonitor {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-#[derive(Debug, Clone)]
-enum Message {
-    Test,
-    TestInput(String),
-}
-
-struct Search {
-    placeholder: String,
-}
-
-#[derive(Debug, Clone)]
-struct Filter {
-    option: combo_box::State<FilterOptions>,
-}
-
-#[derive(Debug, Clone)]
-enum FilterOptions {
-    One,
-}
-
-struct Refresh;
 
 impl TarnerMonitor {
 
-    fn new() -> Self {
-        Self
-    }
+    fn new(
+        selected_process: Option<Pid>,
+        seach_str: String,
+        total_memory: u64,
+        cpu_len: usize,
+    ) -> Self {
 
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::Test => println!("Test"),
-            _ => println!("Test Input"),
+        TarnerMonitor {
+            processes: Vec::new(),
+            selected_process,
+            seach_str,
+            total_memory,
+            cpu_len,
         }
     }
 
-    fn view(&self) -> Element<'_, Message, Theme> {
-
-        let search = Search {
-            placeholder: String::from("Type to search..."),
-        };
-        
-        let header = container(
-            row![
-                text("TarnerMonitor"),
-                horizontal_space(),
-                button("Settings").on_press(Message::Test),
-                button("Export").on_press(Message::Test),
-            ]
-            .spacing(10)
-        )
-        .padding(10);
-
-        let sfr = container(
-            row![
-                horizontal_space(),
-                text("Search: "),
-                text_input("Search", &search.placeholder).on_input(move |input: String| Message::TestInput(input)),
-                horizontal_space(),
-                button("End Task").on_press(Message::Test),
-                horizontal_space(),
-                text("Filter: "),
-                horizontal_space(),
-                text("Refresh: "),
-            ]
-        )
-        .padding(10);
-
-        column![
-            header,
-            sfr
-        ]
-        .into()
+    fn add(&mut self, process: ProcessInfo) {
+        self.processes.push(process);
     }
+
+    fn select_process(&mut self, process: ProcessInfo) {
+        self.selected_process = Some(process.pid);
+    }
+
+    fn get_filtered(&self, filter: &str) -> Vec<&ProcessInfo> {
+        self.processes
+            .iter()
+            .filter(|x| {
+                x.name
+                    .to_string_lossy()
+                    .to_lowercase()
+                    .contains(&filter.to_lowercase())
+            })
+            .collect()
+    }
+
+}
+#[derive(Clone)]
+struct ProcessInfo {
+    name: OsString,
+    parent_pid: Option<Pid>,
+    pid: Pid,
+    cpu_usage: f32,
+    memory_usage: u64,
 }
 
+#[derive(Debug)]
+enum Message{
+    // ProcessSelected(Pid),
+    // SearchChanged(String),
+    // KillProcess,
+    // RefreshProcesses,
+    SortAlpha,
+    SortCpuA,
+    SortCpuD,
+    SortMemA,
+    SortMemD,
+}
 
+fn main() {
+    let mut sys = System::new_all();
+    sys.refresh_all();
 
+    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+    // Refresh CPU usage to get actual value.
+    sys.refresh_all();
+    
+
+    let mut tarnermonitor = TarnerMonitor::new(
+        None,
+        String::new(),
+        sys.total_memory(),
+        sys.cpus().len()
+    );
+
+    for (pid, process) in sys.processes() {
+        tarnermonitor.add(ProcessInfo { name: process.name().to_os_string(), parent_pid: process.parent(), pid: *pid, cpu_usage: process.cpu_usage(), memory_usage: process.memory() });
+    }
+
+    let search_term = "ope";
+    let mut filtered_processes = tarnermonitor.get_filtered(search_term);
+
+    let filt = Message::SortAlpha;
+    sys.refresh_cpu_all();
+    match filt {
+        Message::SortAlpha => filtered_processes.sort_by(|a, b| a.name.cmp(&b.name)),
+        Message::SortCpuA => filtered_processes.sort_by(|a, b| a.cpu_usage.partial_cmp(&b.cpu_usage).unwrap_or(std::cmp::Ordering::Equal)),
+        Message::SortMemA => filtered_processes.sort_by(|a, b| a.memory_usage.cmp(&b.memory_usage)),
+        Message::SortCpuD => filtered_processes.sort_by(|a, b| b.cpu_usage.partial_cmp(&a.cpu_usage).unwrap_or(std::cmp::Ordering::Equal)),
+        Message::SortMemD => filtered_processes.sort_by(|a, b| b.memory_usage.cmp(&a.memory_usage)),
+    }
+
+    for process in filtered_processes {
+        println!("{:?}, CPU: {:.3}%, Memory: {:.3}%", process.name, process.cpu_usage / tarnermonitor.cpu_len as f32, (process.memory_usage as f64 / tarnermonitor.total_memory as f64) * 100.0)
+    }
+}
