588d784c6e6be87d31d26ebd69b51cd7ec331030
TUNERXD <127730775+TUNERXD@users.noreply.github.com>
2025-11-05 22:49:19 +0700

    completed 2 TODO

--- src/view.rs
+++ src/view.rs
@@ -2,8 +2,6 @@ use crate::state::{Message, TarnerMonitor, AppTheme};
 use iced::widget::{button, column, container, row, scrollable, text, text_input, Column};
 use iced::{Element, Length, Theme};
 
-// TODO: Refractor Code -> Selected Process: Option<ProcessInfo>
-// TODO: Process Detail below
 // TODO: Another window for Computer Detail
 // TODO: Confirm when Kill process
 // TODO: Export Processes to CSV
